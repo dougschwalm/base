0ed174e0928cbd673d2ef3d50351d676c1e29ea8
dougschwalm <39537492+dougschwalm@users.noreply.github.com>
2018-11-01 16:49:32 -0500

    Create eda_univariate.R

---
+++ R/eda_univariate.R
@@ -0,0 +1,58 @@
+# this file defines EDA_do ,takes a list of variables, and performs typical EDA on them
+
+# histogram, density plot, boxplot, and Q-Q plot.
+
+#loop through the variables
+
+
+#if structure to check for type of variable
+
+#if it is a factor
+
+# if it is a text-based factor
+#<10
+barchart
+piechart
+frequencies
+missing
+#>10
+barchart
+frequencies
+missing
+
+
+# if text based binary
+
+piechart
+frequencies
+missing
+
+# if it is a numerical factor
+
+#<10
+barchart
+piechart
+frequencies
+missing
+#>10
+barchart
+frequencies
+missing
+
+# if numeric binary
+
+piechart
+frequencies
+missing
+
+# if continuous
+
+density
+boxplot
+q-qplot
+sumstats
+missing
+
+
+
+
